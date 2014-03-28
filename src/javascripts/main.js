6cc87fd980913d8387d7a01cd9a8931d3fdd3933
Andy VanEe <1andyvanee@gmail.com>
2014-03-28 17:00:14 -0600

    Basic toggle functionality for areas

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,3 +1,8 @@
 $(function(){
-  console.log('ready');
+  $('.next-steps .block-inner > h4').not('#summary').each(function(id, elem){
+    $(elem).next('p').toggleClass('hidden');
+    $(elem).click(function(ev){
+      $(ev.currentTarget).next('p').toggleClass('hidden');
+    });
+  });
 });
